158ec2dc4b803ca5fe47d305760cb9d6e50975c9
rakibjrb <rakibjrb572157@gmail.com>
2024-12-04 11:10:34 +0600

    implemented nodemailer for receive a email

--- src/controllers/api/contact/postContact.js
+++ src/controllers/api/contact/postContact.js
@@ -1,10 +1,44 @@
+require("dotenv").config();
 const Contacts = require("../../../models/contact/contact");
+const smtp = require("nodemailer");
+
+const sendMail = (user) => {
+  const transporter = smtp.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: "465",
+    ssl: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const transporterOptions = {
+    to: `${process.env.SEND_TO}`,
+    subject: `${user?.subject}`,
+    text: `${user.message}`,
+    html: `<h2>Name : ${user?.name || "Unkown"}</h2>
+    <h3>Email Address : ${user?.email}</h3>
+    <h4>Message : ${user?.message}</h4>
+    `,
+  };
+
+  transporter.sendMail(transporterOptions, (err, info) => {
+    if (err) {
+      return;
+    } else {
+      return;
+    }
+  });
+};
 
 const postContact = async (req, res, next) => {
   try {
     const contactData = req.body;
     const posted = await Contacts.create(contactData);
     res.send(posted);
+    sendMail(contactData);
   } catch (error) {
     next(error);
   }
